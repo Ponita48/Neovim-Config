b362396c435f79862cb65e389fe3a38e6c4f2dc6
Dwiki Nurkurniawan <dwiki.nsputra@paracorpgroup.com>
2025-09-04 17:39:09 +0700

    add neo tree, projects, and git gutter

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,18 +1,21 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = true,
-    opts = {
-        window = {
-            position = 'right',
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        },
+        lazy = false,                -- neo-tree will lazily load itself
+        config = true,
+        opts = {
+            window = {
+                position = 'right',
+            },
+        },
+        keys = {
+            { "<leader>nn", ":Neotree toggle position=right<CR>", desc = "Open File Browser", silent = true },
         },
     }
-  }
 }
